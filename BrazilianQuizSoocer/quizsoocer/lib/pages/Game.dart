a54d3b84942306cdaa83c27458131e7a28fb0020
RicMerces <ricardoliveira0703@gmail.com>
2022-04-27 15:31:03 -0300

    Arrow

--- BrazilianQuizSoocer/quizsoocer/lib/pages/Game.dart
+++ BrazilianQuizSoocer/quizsoocer/lib/pages/Game.dart
@@ -10,110 +10,125 @@ class _GameState extends State<Game> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
-            child: Image.asset(
-              'assets/img/Component3.png',
-            ),
-          ),
-          const Text(
-            'Pergunta #01',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 40, right: 40, top: 15, bottom: 30),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit ?',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Column(
+          Row(
             children: [
               Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Pergunta 01',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                      fixedSize: Size(295, 50),
-                      backgroundColor: Color(0xffF4A522),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Pergunta 02',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                      fixedSize: Size(295, 50),
-                      backgroundColor: Color(0xffF4A522),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Pergunta 03',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                      fixedSize: Size(295, 50),
-                      backgroundColor: Color(0xffF4A522),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Pergunta 04',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                      fixedSize: Size(295, 50),
-                      backgroundColor: Color(0xffF4A522),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
+                  padding: EdgeInsets.only(left: 30, top: 20),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: Color(0xff2D19CB),
+                        size: 36,
+                      )))
             ],
-          )
+          ),
+          Column(children: [
+            Center(
+              child: Image.asset(
+                'assets/img/Component3.png',
+              ),
+            ),
+            const Text(
+              'Pergunta #01',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 40, right: 40, top: 15, bottom: 30),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit ?',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Pergunta 01',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        fixedSize: Size(295, 50),
+                        backgroundColor: Color(0xffF4A522),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Pergunta 02',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        fixedSize: Size(295, 50),
+                        backgroundColor: Color(0xffF4A522),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Pergunta 03',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        fixedSize: Size(295, 50),
+                        backgroundColor: Color(0xffF4A522),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Pergunta 04',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        fixedSize: Size(295, 50),
+                        backgroundColor: Color(0xffF4A522),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+              ],
+            ),
+          ])
         ],
       ),
     );
