18022e7a5f04af1de4724ac4a167b16270da22b1
RicMerces <ricardoliveira0703@gmail.com>
2022-04-28 17:36:06 -0300

    Correção bug de lista de perguntas

--- BrazilianQuizSoocer/quizsoocer/lib/pages/Game.dart
+++ BrazilianQuizSoocer/quizsoocer/lib/pages/Game.dart
@@ -8,89 +8,66 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  var _pergunta = 0;
+  var _novaPergunta = 0;
   var _somaTotalPts = 0;
   // var alterarCor = false;
 
   String perguntaAtual = '';
 
-  final List<Map<String, Object>> perguntas = [
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-    {
-      'texto': 'Pergunta',
-      'respostas': [
-        {'texto': 'Opcao01', 'nota': 10},
-        {'texto': 'Opcao02', 'nota': 12},
-        {'texto': 'Opcao03', 'nota': 14},
-        {'texto': 'Opcao04', 'nota': 16},
-      ]
-    },
-  ];
-
-  void aPergunta() {}
-
   void _responder() {
     setState(() {
-      _pergunta++;
-      perguntaAtual = perguntaAtual = perguntas[_pergunta].cast()['texto'];
+      _novaPergunta++;
+      // perguntaAtual = perguntaAtual = _perguntas[_novaPergunta].cast()['texto'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Pergunta5',
+        'respostas': [
+          'Opcao0er3q1',
+          'Opcao0sdf2',
+          'Opcao03asd',
+          'Opcao0r3e4',
+        ]
+      },
+      {
+        'texto': 'Pergunta4',
+        'respostas': [
+          'Opcao05',
+          'Opcao062',
+          'Opcao0773',
+          'Opcao0884',
+        ]
+      },
+      {
+        'texto': 'Pergunta3',
+        'respostas': [
+          'Opcao0345221',
+          'Opcao03452',
+          'Opcao05563',
+          'Opcao0344',
+        ]
+      },
+      {
+        'texto': 'Pergunta2',
+        'respostas': [
+          'Opcao02342341',
+          'Opcao06542',
+          'Opcao0uuu3',
+          'Opcao04444',
+        ]
+      },
+    ];
+
+    List respostas = [];
+
+    for (var textoResp in perguntas[_novaPergunta].cast()['respostas']) {
+      respostas.add(OpcoesModel(textoResp, _responder));
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -114,21 +91,14 @@ class _GameState extends State<Game> {
               ),
             ),
             Text(
-              'Pergunta #${_pergunta + 1}',
+              'Pergunta #${_novaPergunta + 1}',
               style: const TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Questao(perguntaAtual = perguntas[_pergunta].cast()['texto']),
-            Column(
-              children: [
-                OpcoesModel('resposta 01', _responder),
-                OpcoesModel('resposta 02', _responder),
-                OpcoesModel('resposta 03', _responder),
-                OpcoesModel('resposta 04', _responder),
-              ],
-            ),
+            Questao(perguntaAtual = perguntas[_novaPergunta].cast()['texto']),
+            ...respostas,
           ])
         ],
       ),
